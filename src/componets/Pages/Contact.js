e8e5716d0f11f881fd59e1e473e030dd3f0f0f7e
Febin George <you@example.com>
2023-03-06 23:05:14 -0800

    updates

---
+++ src/componets/Pages/Contact.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Contact() {
+  return (
+    <form>
+      <textarea placeholder='What do you want to say?'/>
+      <botton>Submit</botton>
+    </form>
+  );
+}
